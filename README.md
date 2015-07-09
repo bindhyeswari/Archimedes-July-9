c967edfa032f5f0f9cc53b78d8058ec2c673762e
mishra <tech@golivelabs.io>
2015-07-09 13:54:44 -0700

    final

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 #Tasks
 
  - Read up on (HTTP Verbs)[http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html]
+ - Meetup API --> [api](https://secure.meetup.com/meetup_api/console/)
+ - Create a login form
+ - Make an AJAX call (not a form submit), to authenticate a user
+ - Read on open, send, setRequestHeader, getResponseHeader, readyState [ajax](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest)
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,37 @@
 var express = require('express'); // external module
 var fs = require('fs'); // core module
 var uuid = require('uuid');
+var request = require('request');
 
 var router = express.Router();
+
+var users = {
+    amy: {
+        password: '',
+        email: ''
+    }
+}; // key: username, value: { password: '', email: '' }
+
+var events = [];
+var valid_api_call = false;
+
+/**
+ * Meetup API Code
+ * */
+/*===========Start Meeetup API Code===========*/
+// todo: Update API Key
+request('https://api.meetup.com/2/events?group_id=18560637&key=22', function (err, response, body) {
+    console.log(response.statusCode);
+    if (response.statusCode === 200) {
+        var response_object = JSON.parse(body);
+        valid_api_call = true;
+        events = response_object.results;
+    } else {
+        valid_api_call = false;
+    }
+});
+
+/*===========End Meeetup API Code===========*/
 
 var jsobject = {
     name: 'info'
@@ -23,18 +52,32 @@ router.get('/json', function (req, res) {
    res.status(200).json(jsobject);
 });
 
-router.get('/login', function (req, res) {
-   res.render('login', {
+router.get('/register', function (req, res) {
+   res.render('register', {
        message: 'Register here ... '
    });
 });
 
-router.post('/login', function (req, res) {
+router.post('/register', function (req, res) {
     // Access the details of the request by accessing the body of the request
-    console.log(req.body);
-    res.render('login', {
+    console.log(req.body.username);
+    res.render('register', {
         message: 'IMPL_101 Registration ... '
     });
+});
+
+router.get('/meetup', function (req, res) {
+   res.render('meetup');
+});
+
+router.get('/meetup/events', function (req, res) {
+    if (valid_api_call) {
+        res.status(200).json(events);
+    } else {
+        res.status(500).json({
+            message: 'Havent got the events data from meetup yet!'
+        });
+    }
 });
 
 
